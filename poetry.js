566d5e063ca7ef1ba6492b8c18f217b4730cce27
Calvin Higdon <calvindhigdon@Gmail.com>
2024-11-11 01:49:05 -0500

    added a poem!

--- poetry.js
+++ poetry.js
@@ -1,13 +1,38 @@
-function changePoemHTML() {
-    var poem = document.getElementById("clickButton");
-    poem.innerHTML = `
-    <p>a gnarled poem. hurtling out of soft
-lungs. a leave, making breath from absence.
-following alone so little and alone, scrape
-out my stomach. i think ive only been
-a child. and a dark oak flute, i am so
-timid. i have only been. pure of light, and
-down. shambling. in some heart. eating breakfast.
-clutch. dinner.</p>
-    `;
+function changePoemHTML(buttonID) {
+    var button = document.getElementById(buttonID);
+    var poemTitle = button.getElementsByClassName("poetry-title")[0];
+    var blurb = button.getElementsByClassName("poetry-text")[0];
+    var poemFull = button.getElementsByClassName("poetry-full")[0];
+    var arrows = button.getElementsByClassName("arrow")
+
+    if (button.classList.contains("selectedButton")) { // If the button is already selected
+        button.classList.remove("selectedButton");
+        blurb.style.display = "flex";
+        poemFull.style.display = "none";
+        for (var i = 0; i < arrows.length; i++) {
+            arrows[i].innerHTML = "&#11167";
+            arrows[i].classList.remove("arrowOpen");
+        }
+        button.setAttribute("style", "height: 7rem");
+    } else { // If the button is not selected
+        button.classList.add("selectedButton");
+        blurb.style.display = "none";
+        poemFull.style.display = "flex";
+        for (var i = 0; i < arrows.length; i++) {
+            arrows[i].classList.add("arrowOpen");
+        }
+        button.setAttribute("style", "height: " + (poemTitle.clientHeight + poemFull.clientHeight + 15) + "px");
+    }
+}
+
+function hoverButton(buttonID) {
+    var button = document.getElementById(buttonID);
+    var arrows = button.getElementsByClassName("arrow")
+
+    for (var i = 0; i < arrows.length; i++) {
+        if (arrows[i].classList.contains("hoverArrow"))
+            arrows[i].classList.remove("hoverArrow");
+        else
+            arrows[i].classList.add("hoverArrow");
+    }
 }
