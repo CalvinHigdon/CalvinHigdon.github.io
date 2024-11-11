8fead1bd5b003336d8196bccbe1a03693ec4d4ab
Calvin Higdon <calvindhigdon@Gmail.com>
2024-11-10 21:46:08 -0500

    progress

---
+++ poetry.js
@@ -0,0 +1,13 @@
+function changePoemHTML() {
+    var poem = document.getElementById("clickButton");
+    poem.innerHTML = `
+    <p>a gnarled poem. hurtling out of soft
+lungs. a leave, making breath from absence.
+following alone so little and alone, scrape
+out my stomach. i think ive only been
+a child. and a dark oak flute, i am so
+timid. i have only been. pure of light, and
+down. shambling. in some heart. eating breakfast.
+clutch. dinner.</p>
+    `;
+}
